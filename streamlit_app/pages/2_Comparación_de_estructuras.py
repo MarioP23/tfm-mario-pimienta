0cc3dbba85332089d965e78bdcbe6875ccf9e3fc
MarioP23 <mpimientacalderon@gmail.com>
2024-06-28 20:24:06 +0200

    Fix button

--- streamlit_app/pages/2_Comparación_de_estructuras.py
+++ streamlit_app/pages/2_Comparación_de_estructuras.py
@@ -16,7 +16,7 @@ def main():
     # Carga del archivo FASTA
     fasta_file = st.file_uploader("Sube tu fichero FASTA", type=["fasta", "fa"])
     if fasta_file:
-        if st.button("Plegar"):
+        if st.button("Plegar FASTA"):
             with st.spinner("Plegando la secuencia del archivo FASTA..."):
                 try:
                     fasta_content = fasta_file.read().decode("utf-8")
@@ -99,7 +99,7 @@ def main():
         #selected_variant = st.selectbox("Selecciona la variante", options=st.session_state.df_variants.index)
         selected_variant = st.selectbox("Selecciona la variante", options=st.session_state.df_variants.index, format_func=lambda x: st.session_state.df_variants.at[x, 'ID'])
 
-        if st.button("Plegar FASTA"):
+        if st.button("Plegar Mutated FASTA"):
             with st.spinner("Plegando la secuencia seleccionada..."):
                 try:
                     selected_variant_data = st.session_state.df_variants.loc[selected_variant]
